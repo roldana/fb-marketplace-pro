a20cf8ff00143166a2f010920ff015bea83146ea
Alejandro <alrolja@gmail.com>
2025-06-07 19:55:33 -0400

    hiding missing top banner

--- renderer.js
+++ renderer.js
@@ -23,6 +23,27 @@ document.addEventListener("DOMContentLoaded", () => {
     console.error("webview element not found");
     }
 
+    // Hide missing top banner
+    webview.addEventListener('dom-ready', () => {
+    const hideHeightRem = 1.8;
+    
+    // Move the whole document body and webview sidebar upward so the first 100px are off-screen.
+    webview.insertCSS(`
+        html, body {
+            margin-top: -${hideHeightRem}rem !important;
+            height: calc(100% + ${hideHeightRem}rem) !important;
+        }
+
+        /* Move the sidebar up */
+        div[aria-label="Marketplace sidebar"] > div:first-of-type {
+            margin-top: -${hideHeightRem+1.2}rem !important;
+            height: calc(100% + ${hideHeightRem+1.2}rem) !important;
+        }
+        
+        `);
+
+
+});
     // Check for permission and request if not granted
     if (Notification.permission !== "granted") {
     Notification.requestPermission().then(permission => {
